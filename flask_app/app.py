7926c3ea37c7beda648f8548df8e1b12181e2203
quasi-researcher <and-golovin@yandex.ru>
2023-04-10 21:32:52 +0200

    only classification

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-import base64
+from flask import Flask, render_template, request, url_for, redirect, session, flash
 import requests as req
 app = Flask(__name__, template_folder='templates')
 app.secret_key = "fake secret key for test purposes"
@@ -7,30 +6,38 @@ app.secret_key = "fake secret key for test purposes"
 
 @app.route('/')
 def health():
-    return render_template("index.html", torch_status='unknown')
+    predicted_label = session.get('predicted_label') or "not executed"
+    torch_status = session.get('torch_status') or "unknown"
+    return render_template("index.html", torch_status=torch_status)
 
 
 @app.route('/ping')
 def ping_server():
     url = "http://127.0.0.1:8080/ping"
     response = req.post(url)
-    return render_template("index.html", torch_status=response.json()['status'])
+    predicted_label = session.get('predicted_label') or "not executed"
+    session['torch_status'] = response.json()['status']
+    return render_template("index.html", torch_status=response.json()['status'], predicted_label=predicted_label)
 
 
-@app.route('/generate', methods=['POST'])
+@app.route('/classify', methods=['POST'])
 def get_prediction():
-    if request.files:
-        content_img = request.files['content'].read()
-        style_img = request.files['style'].read()
+    image = request.files['selected_image'].read()
 
-        url = "http://localhost:8080/predictions/nst"
-
-        response = req.post(url, data={'content': content_img, 'style': style_img})
-        result = response.content
-        return {'out_img': base64.b64encode(result).decode()}
+    url = "http://127.0.0.1:8080/predictions/fmnist"
+    if image:
+        response = req.post(url, data={'body': image})
+        session['predicted_label'] = response.text
+        return render_template("result.html", predicted_label=response.text)
     else:
-        result = 'Please, select an image'
-        return {'error': result}
+        flash('Select an image')
+    return redirect(url_for("health"))
+
+
+@app.route('/back')
+def back_to_main():
+    session['predicted_label'] = 'not executed'
+    return redirect(url_for("health"))
 
 
 if __name__ == "__main__":
